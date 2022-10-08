7b213e883fe1e44857576b93f013f23afd2fa485
mo <nataleshen.ko@yandex.ru>
2022-10-08 09:57:21 +0300

    my homework

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("ветка master");
+        Author author1 = new Author("Роберт", "Мартин");
+        Author author2 = new Author("Николай", "Прохоренок");
+        //System.out.println("author2.authorSurname = " + author2.getAuthorSurname());
+        Book book1 = new Book("Чистый Код", author1, 2022);
+        Book book2 = new Book("Основы JAVA", author2, 2019);
+        System.out.println(book1.getBookName() + " " + book1.getAuthor().getFirstName() + " " + book1.getAuthor().getSecondName()+ " " + book1.getPublishingYear());
+        System.out.println(book2.getBookName() + " " + book2.getAuthor().getFirstName() + " " + book2.getAuthor().getSecondName()+ " " + book2.getPublishingYear());
+        book1.setPublishingYear(2021);
+        System.out.println("book1.age = " + book1.getPublishingYear());
+
+
     }
 }
